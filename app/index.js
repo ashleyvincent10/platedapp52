fb8f48fb041e86b9c99e8346e514407cd7ee4968
Ashley Vincent <ashleyv1@stanford.edu>
2024-11-21 17:58:05 -0800

    ITS WORKING

--- app/index.js
+++ app/index.js
@@ -17,7 +17,7 @@ export default function App() {
   }, [fontsLoaded]);
   if (!fontsLoaded) return null;
 
-  return <Redirect href="/login" onLayout={onLayoutRootView} />;
+  return <Redirect href="/profile" onLayout={onLayoutRootView} />;
 }
 
 //font export lines from A2
