2f9e61c6f62309d25ba840c68af8c09316302bcf
Ashley Vincent <ashleyv1@stanford.edu>
2024-11-21 18:08:58 -0800

    use session commented out

--- utils/useSession.js
+++ utils/useSession.js
@@ -1,29 +1,25 @@
 /**
- * Hook that returns the current Supabase auth session.
- * Starts as null and returns the session once it's found.
- * If the user is not logged in, returns null.
+ *CHANGE THIS WHEN WE IMPLEMENT LOGIN PAGE
  */
 
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-// import db from "@/database/db";
+// export default function useSession() {
+//   const [session, setSession] = useState(null);
 
-export default function useSession() {
-  const [session, setSession] = useState(null);
+//   useEffect(() => {
+//     db.auth.getSession().then(({ data: { session } }) => {
+//       setSession(session);
+//     });
 
-  useEffect(() => {
-    db.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
+//     const {
+//       data: { subscription },
+//     } = db.auth.onAuthStateChange((_event, session) => {
+//       setSession(session);
+//     });
 
-    const {
-      data: { subscription },
-    } = db.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
+//     return () => subscription.unsubscribe();
+//   }, []);
 
-    return () => subscription.unsubscribe();
-  }, []);
-
-  return session;
-}
+//   return session;
+// }
