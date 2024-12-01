0d87c56b4cf101e5e00c84a9b547271341c3d352
Ashley Vincent <ashleyv1@stanford.edu>
2024-12-01 15:35:40 -0500

    Progress on pantry

--- app/(tabs)/pantry/index.js
+++ app/(tabs)/pantry/index.js
@@ -1,28 +1,114 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { useRouter } from "expo-router";
+import { useState, useEffect } from "react";
 
-import { Link } from "expo-router";
+import { supabase } from "backend/supabaseClient";
+
+// const windowWidth = Dimensions.get("window").width;
 
 export default function Page() {
+  const [items, setItems] = useState(null);
+  const router = useRouter();
+
+  const fetchItems = async () => {
+    try {
+      const user_response = await supabase.from("Pantry").select("*");
+      setItems(user_response.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  console.log(items);
+
+  const renderHeader = () => (
+    <View style={styles.itemContainer}>
+      <Text style={styles.itemLabel}>This is the top item!</Text>
+    </View>
+  );
+
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Pantry</Text>
-      </View>
+    <View style={styles.main}>
+      <FlatList
+        data={items}
+        keyExtractor={(item) => item.id}
+        ListHeaderComponent={renderHeader}
+        scrollEnabled={true}
+        numColumns={2}
+        renderItem={({ item }) => (
+          <TouchableOpacity style={styles.itemContainer}>
+            <Text style={styles.itemLabel}>{item.name}</Text>
+
+            {/* <Image source={{ uri: item.imageUrl }} style={styles.albumImage} />
+            <View style={styles.songInfo}>
+              <Text
+                style={styles.songTitle}
+                numberOfLines={1}
+                ellipsizeMode="tail"
+              >
+                {item.songTitle}
+              </Text>
+              <Text
+                style={styles.artist}
+                numberOfLines={1}
+                ellipsizeMode="tail"
+              >
+                {item.songArtists.map((artist) => artist.name).join(", ")}
+              </Text>
+            </View>
+            <Text style={styles.album} numberOfLines={1} ellipsizeMode="tail">
+              {item.albumName}
+            </Text>
+            <Text style={styles.duration}>
+              {millisToMinutesAndSeconds(item.duration)}
+            </Text> */}
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
   main: {
     flex: 1,
+    alignItems: "center",
+    // padding: 12,
     justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
+    alignItems: "center",
+  },
+  itemContainer: {
+    width: 166,
+    height: 166,
+    borderWidth: 2,
+    borderColor: "#B5300B",
+    marginTop: 20,
+    marginBottom: 20,
+    marginRight: 10,
+    marginLeft: 10,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    backgroundColor: "#FAF9F6",
+  },
+  itemLabel: {
+    color: "black",
+    fontFamily: "Prata-Regular",
+    fontSize: 16,
+    fontWeight: 400,
+    margin: 4,
   },
   title: {
     fontSize: 64,
