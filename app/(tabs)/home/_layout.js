1d68c799e25b3c0b9e1948ede58dc84d09545c19
Ashley Vincent <ashleyv1@stanford.edu>
2024-12-05 20:28:02 -0800

    Push

--- app/(tabs)/home/_layout.js
+++ app/(tabs)/home/_layout.js
@@ -5,40 +5,41 @@ import { FilterProvider } from "./FilterContext";
 export default function StackLayout() {
   const router = useRouter();
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{
-          headerShown: false,
-          headerTitle: " ",
-          headerStyle: {
-            backgroundColor: "#FAF9F6",
-          },
-          headerTitleStyle: {
-            fontFamily: "Prata-Regular",
-            fontSize: 34,
-            color: "black",
-          },
-        }}
-      />
+    <FilterProvider>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            headerShown: false,
+            headerTitle: " ",
+            headerStyle: {
+              backgroundColor: "#FAF9F6",
+            },
+            headerTitleStyle: {
+              fontFamily: "Prata-Regular",
+              fontSize: 34,
+              color: "black",
+            },
+          }}
+        />
 
-      <Stack.Screen
-        name="filters"
-        options={{
-          headerShown: true,
-          headerStyle: {
-            backgroundColor: "#FAF9F6",
-          },
-          headerTitle: "Set Filters",
-          headerTitleStyle: {
-            fontFamily: "Prata-Regular",
-            fontSize: 34,
-            color: "black",
-          },
-          headerTintColor: "black",
-        }}
-      />
-      <Stack.Screen name="recipe_details1" />
-    </Stack>
+        <Stack.Screen
+          name="filters"
+          options={{
+            headerShown: true,
+            headerStyle: {
+              backgroundColor: "#FAF9F6",
+            },
+            headerTitle: "Set Filters",
+            headerTitleStyle: {
+              fontFamily: "Prata-Regular",
+              fontSize: 34,
+              color: "black",
+            },
+            headerTintColor: "black",
+          }}
+        />
+      </Stack>
+    </FilterProvider>
   );
 }
